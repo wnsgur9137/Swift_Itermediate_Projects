51a6d2ba5be10bada03a795a047ec81eb738bcb6
Junhyeok <83490421+wnsgur9137@users.noreply.github.com>
2022-09-10 02:27:50 +0900

    Brewery UI

---
+++ Brewery/Brewery/Beer.swift
@@ -0,0 +1,34 @@
+//
+//  Bear.swift
+//  Brewery
+//
+//  Created by 이준혁 on 2022/09/10.
+//
+
+import Foundation
+
+struct Beer: Decodable {
+    let id: Int?
+    let name, taglineString, description, brewersTips, imageURL: String?
+    let foodParing: [String]?
+    
+    var tagLine: String {
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map {
+            "#" + $0.replacingOccurrences(of: " ", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: " #")
+        }
+        return hashtags?.joined(separator: " ") ?? ""   // #tag #good #hello
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description
+        case taglineString = "tagline"
+        case imageURL =  "image_url"
+        case brewersTips = "brewers_tips"
+        case foodParing = "food_paring"
+    }
+}
+
+
